35d3c5e1d3303aae9f53fd8464e6756d30180e9c
tsvalencio-IA <tsvalencio@gmail.com>
2026-05-13 16:29:02 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// CONFIGURAÇÃO DO FIREBASE
+// Opção 1: cole aqui a configuração do seu Firebase Web App e suba para o GitHub Pages.
+// Opção 2: deixe como está e cole a configuração direto na tela do sistema; o link do vendedor levará a config no próprio link.
+window.firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI.firebaseapp.com",
+  databaseURL: "https://COLE_AQUI-default-rtdb.firebaseio.com",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI.appspot.com",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
