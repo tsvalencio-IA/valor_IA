3149e82f9aa192bac6d3fbe7ba3edbc82c381507
tsvalencio-IA <tsvalencio@gmail.com>
2026-05-13 16:40:53 -0300

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // CONFIGURAÇÃO DO FIREBASE
 // Opção 1: cole aqui a configuração do seu Firebase Web App e suba para o GitHub Pages.
 // Opção 2: deixe como está e cole a configuração direto na tela do sistema; o link do vendedor levará a config no próprio link.
-window.firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  databaseURL: "https://COLE_AQUI-default-rtdb.firebaseio.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+const firebaseConfig = {
+  apiKey: "AIzaSyAJDq18thvCzDsjTGRsJLmQQ0DuXuZFA5c",
+  authDomain: "valoria-70900.firebaseapp.com",
+  databaseURL: "https://valoria-70900-default-rtdb.firebaseio.com",
+  projectId: "valoria-70900",
+  storageBucket: "valoria-70900.firebasestorage.app",
+  messagingSenderId: "776990151458",
+  appId: "1:776990151458:web:912775c07abd3c3d94cb7b"
 };
